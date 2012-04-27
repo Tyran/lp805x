c992758cd6adf9c7d4e707a34ca4a2c22083b199
Tiago Castro <tiagolobocastro@gmail.com>
2012-04-27 18:57:45 +0100

    somes testing

--- rtl/verilog/oc8051_defines.v
+++ rtl/verilog/oc8051_defines.v
@@ -88,11 +88,11 @@
 
 //NONE-INFERED
 //`define LP805X_ROM_INFER
-//`define LP805X_XILINX
-`define LP805X_ALTERA
+`define LP805X_XILINX
+//`define LP805X_ALTERA
 
 //clock management
-`define LP805X_CLKER
+//`define LP805X_CLKER
 
 //
 // oc8051 simulation defines
