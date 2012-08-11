e9fad862b37a00e1a169646c1686b29cf2921fd6
Tiago Castro <tiagolobocastro@gmail.com>
2012-08-11 02:08:06 +0100

    integrating sync into ports and testing

--- rtl/verilog/lp805x_sfrbus.v
+++ rtl/verilog/lp805x_sfrbus.v
@@ -29,11 +29,10 @@ module lp805x_sfrbuse(
     input wr_bit,
     input rd_bit,
 	 input load,
-	 output [28:0] sfr_bus
+	 output reg [28:0] sfr_bus
     );
 	 
 	 wire [28:0] sfr_bus_;
-	 reg [28:0] sfr_bus;
 	
 	assign 
 		sfr_bus_ = { wr_addr, rd_addr, data_in, 
@@ -44,16 +43,33 @@ module lp805x_sfrbuse(
 			sfr_bus <= #1 sfr_bus_;
 endmodule
 
+module lp805x_sfrbused(
+    output [7:0] wr_addr,
+    output [7:0] rd_addr,
+    output [7:0] data_in,
+    output wr,
+    output rd,
+    output bit_in,
+    output wr_bit,
+    output rd_bit,
+	 input [28:0] sfr_bus
+    );
+	
+	assign 
+		{ wr_addr, rd_addr, data_in, 
+			wr, rd, bit_in, wr_bit, rd_bit } = sfr_bus;
+
+endmodule
+
 module lp805x_sfrbusd(
 	 input clk,
     input [7:0] data_out,
     input bit_out,
 	 input load,
-	 output [8:0] sfr_bus
+	 output reg [8:0] sfr_bus
     );
 	 
 	 wire [8:0] sfr_bus_;
-	 reg [8:0] sfr_bus;
 	
 	assign 
 		sfr_bus_ = { data_out, bit_out };
---
+++ rtl/verilog/lp805x_synctrl.v
@@ -0,0 +1,83 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    11:05:33 08/10/2012 
+// Design Name: 
+// Module Name:    lp805x_synctrl 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module lp805x_synctrl(
+		clk,
+		rst,
+		read,
+		sfr_prrdy,
+		sfr_pget,
+		sfr_pwrdy,
+		sfr_pput,
+		clk_cpu,
+		sfr_get,
+		sfr_out
+    );
+	 
+	input clk,rst;
+	input read;
+	input sfr_prrdy,sfr_pwrdy;
+	output sfr_pget,sfr_pput;
+		
+	input clk_cpu;
+	input sfr_get;
+	output sfr_out;
+	
+	reg sfr_preput;
+	reg sfr_pget,sfr_pput;
+	
+	reg sfr_out;
+
+	always @(posedge clk)
+	if ( rst)
+		sfr_pput <= #1 0;
+	else
+		sfr_pput <= #1 sfr_preput;
+			
+	always @(posedge clk)
+	begin
+		if ( rst)
+		begin
+			sfr_pget <= #1 0;
+			sfr_preput <= #1 0;
+		end
+		else if ( sfr_prrdy)
+		begin
+			sfr_pget <= #1 1;
+			sfr_preput <= #1 0;
+		end
+		else if ( sfr_pget & sfr_pwrdy & read)
+		begin
+			sfr_pget <= #1 0;
+			sfr_preput <= #1 1;
+		end
+		else if ( sfr_preput)
+		begin
+			sfr_preput <= #1 0;
+		end
+	end
+	
+	always @(posedge clk_cpu)
+	if ( rst)
+		sfr_out <= #1 1'b0;
+	else
+		sfr_out <= #1 sfr_get;
+
+endmodule
